f8a59e5dddae5e0c22b341d243f6db67853fbbbb
simsrpcollabs-byte <simsrpcollabs@gmail.com>
2026-09-03 16:52:28 -0500

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,19 +1,25 @@
 const { REST, Routes } = require('discord.js');
 const config = require('./config');
 const commands = require('./commands');
+const db = require('./database');
 
 async function deploy() {
   const rest = new REST({ version: '10' }).setToken(config.token);
   const body = commands.map((command) => command.data.toJSON());
+
   console.log(`Deploying ${body.length} VERA commands to server ${config.guildId}...`);
+
   const result = await rest.put(
     Routes.applicationGuildCommands(config.clientId, config.guildId),
     { body },
   );
+
   console.log(`Success: ${result.length} commands deployed.`);
 }
 
-deploy().catch((error) => {
-  console.error('Command deployment failed:', error);
-  process.exitCode = 1;
-});
+deploy()
+  .catch((error) => {
+    console.error('Command deployment failed:', error);
+    process.exitCode = 1;
+  })
+  .finally(() => db.close());
